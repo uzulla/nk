2da119975099c63801311983908d3f58a7c35a35
uzulla <uzulla@himitsukichi.com>
2020-10-02 08:11:11 +0900

    add mysql2 sample

---
+++ mysql2/insert.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const mysql = require("mysql2");
+const util = require("util");
+const promisify = util.promisify;
+
+const dbinfo = {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "isucon",
+    password: "isucon",
+    database: "isuumo",
+    connectionLimit: 10,
+};
+
+const db = mysql.createPool(dbinfo);
+
+(async () => {
+    const getConnection = promisify(db.getConnection.bind(db));
+    const connection = await getConnection();
+    const beginTransaction = promisify(connection.beginTransaction.bind(connection));
+    const query = promisify(connection.query.bind(connection));
+    const commit = promisify(connection.commit.bind(connection));
+    const rollback = promisify(connection.rollback.bind(connection));
+    try {
+        await beginTransaction();
+
+        const res = await query(
+            "INSERT INTO test(text, num) VALUES(?,?)",
+            ["text", 1]
+        )
+
+        console.log(res);
+        console.log(res.insertId);
+
+        await commit();
+        console.log("ok")
+
+    } catch (e) {
+        await rollback();
+        console.log(e);
+    } finally {
+        await connection.release();
+        process.exit(0);
+    }
+})();
+
+
+
